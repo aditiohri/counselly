5dd0ff17f09afb3dcd0c9c72afb08d44f0387324
Aditi Ohri <didiohri@gmail.com>
2020-01-17 17:02:58 -0600

    add token-based-auth

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,27 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom';
+import userService from '../../utils/userService';
+import SignupPage from '../../pages/SignupPage/SignupPage'
+import LoginPage from '../../pages/LoginPage/LoginPage'
+import HomePage from '../HomePage/HomePage'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: userService.getUser()
+    };
+  }
+
+  handleLogout = () => {
+    userService.logout();
+    this.setState({ user: null });
+  };
+
+  handleSignuporLogin = () => {
+    this.setState({ user: userService.getUser() });
+  };
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <HomePage
+                user={this.state.user}
+                handleLogout={this.handleLogout}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/signup"
+            render={({ history }) => (
+              <SignupPage
+                history={history}
+                handleSignuporLogin={this.handleSignuporLogin}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/login"
+            render={({ history }) => (
+              <LoginPage
+                history={history}
+                handleSignuporLogin={this.handleSignuporLogin}
+              />
+            )}
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "../../components/NavBar/NavBar";
+// import "./HomePage.css";
+
+const HomePage = props => {
+  return (
+    <div className="HomePage">
+      <NavBar user={props.user} handleLogout={props.handleLogout} />
+    </div>
+  );
+};
+
+export default HomePage;
