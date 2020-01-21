9017536c7dc5eb00678c6326c9aa1bcec6e89a52
Aditi Ohri <didiohri@gmail.com>
2020-01-21 16:13:30 -0600

    add delete one functionality

--- routes/api/appointments.js
+++ routes/api/appointments.js
@@ -8,5 +8,6 @@ router.use(require('../../config/auth'));
 router.get("/", apptCtrl.index);
 router.get('/:id', apptCtrl.show)
 router.post("/", apptCtrl.create);
+router.delete('/:id', apptCtrl.delete)
 
 module.exports = router;
